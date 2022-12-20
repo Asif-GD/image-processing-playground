c31175de79d50fba7d0ca883b464230edc2b5f58
Asif-GD <asif.coach@gmail.com>
2022-12-20 16:32:33 +0530

    moved the manipulated images to a new directory
    - resultant-images

--- image-manipulation.py
+++ image-manipulation.py
@@ -4,13 +4,13 @@ sample_image = Image.open("./sample-images/pikachu.jpg")
 # print(f"sample_image -> {sample_image}")
 
 filtered_image = sample_image.filter(ImageFilter.BLUR)
-filtered_image.save("./sample-images/pikachu_blur.png", format="png")
+filtered_image.save("./resultant-images/pikachu_blur.png", format="png")
 
 filtered_image = sample_image.filter(ImageFilter.SHARPEN)
-filtered_image.save("./sample-images/pikachu_sharpen.png", format="png")
+filtered_image.save("./resultant-images/pikachu_sharpen.png", format="png")
 
 # print(dir(Image))
 # print(dir(sample_image))
 
 filtered_image = sample_image.convert('L')
-filtered_image.save("./sample-images/pikachu_grey.png", format="png")
+filtered_image.save("./resultant-images/pikachu_grey.png", format="png")
