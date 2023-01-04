dbaf59beb61ea2724c576c1c7edd9fa1290e3e0a
Asif-GD <asif.coach@gmail.com>
2023-01-04 17:59:27 +0530

    used dir() on Image & ImageFilter classes

--- image-manipulation.py
+++ image-manipulation.py
@@ -9,8 +9,8 @@ filtered_image.save("./resultant-images/pikachu_blur.png", format="png")
 filtered_image = sample_image.filter(ImageFilter.SHARPEN)
 filtered_image.save("./resultant-images/pikachu_sharpen.png", format="png")
 
+# print(dir(ImageFilter))
 # print(dir(Image))
-# print(dir(sample_image))
 
 filtered_image = sample_image.convert('L')
 filtered_image.save("./resultant-images/pikachu_grey.png", format="png")
